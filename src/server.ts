54f7d8a40d12f7eb7e9cc17049c4b389bee3d502
Yuri Soares <yurisoaresm@outlook.com>
2023-06-07 21:22:05 -0300

    init server repository

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from 'fastify';
+
+import { knex } from './database';
+
+const app = fastify();
+
+app.get('/schema', async () => {
+  const tables = await knex('sqlite_schema').select('*');
+
+  return tables;
+});
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => console.log(`HTTP Server is running on port 3000`));
